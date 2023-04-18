cbeb35cf1a4ab1f59ad50500c2b31941948fd20b
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-04-18 03:42:28 -0300

    feat(tasks): add getById error handling

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDTO } from './dto/create-task.dto';
 import { GetTasksFilterDTO } from './dto/get-tasks-filter.dto';
@@ -31,7 +31,13 @@ export class TasksService {
   }
 
   getById(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException('Task not found');
+    }
+
+    return task;
   }
 
   create(createTaskDto: CreateTaskDTO): Task {
