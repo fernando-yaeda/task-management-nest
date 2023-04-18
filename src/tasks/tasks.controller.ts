90122fcfacf06cd1b76b08a0cfc20978cd71ebf3
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-04-18 02:18:49 -0300

    feat(tasks): add create new task
    also add uuid to pachage.json

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
@@ -9,5 +9,13 @@ export class TasksController {
   @Get()
   getAll(): Task[] {
     return this.tasksService.getAll();
+  }
+
+  @Post()
+  create(
+    @Body('title') title: string,
+    @Body('description') description: string,
+  ): Task {
+    return this.tasksService.create(title, description);
   }
 }
