a4e69c720c22184bc38ef5670863cb5e5a4b7c63
fernando-yaeda <fernandoyudi.yaeda@gmail.com>
2023-09-21 21:04:09 -0300

    chore(swagger): add initial swagger settings

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
+import 'dotenv/config';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ValidationError, useContainer } from 'class-validator';
-import 'dotenv/config';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
 import { AppModule } from './app.module';
 import { ApiConfigService } from './shared/api-config.service';
 import { SharedModule } from './shared/shared.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('Triangle API')
+    .setDescription('task management api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useLogger(app.get(Logger));
+
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
 
   app.useGlobalPipes(
